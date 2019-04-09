7170e1a0562c0d509d741397e7288f623b3fee65
unknown <abdellalikabou39@gmail.com>
2019-04-09 11:49:21 +0100

    install packeges

---
+++ src/Controller/BabyProductsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BabyProductsController extends AbstractController
+{
+    /**
+     * @Route("/babyProducts", name="baby_products")
+     */
+    public function index()
+    {
+        return $this->render('baby_products/index.html.twig', [
+            'controller_name' => 'BabyProductsController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('baby_products/home.html.twig');
+    }
+
+    /**
+     * @Route("/about-us", name="about")
+     */
+    public function about()
+    {
+        return $this->render('baby_products/about.html.twig');
+    }
+}
