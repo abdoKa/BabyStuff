a43fb70c9df3ed65349acc5d7d87793f7980707b
Ali Kapoor <abdellalikabou39@gmail.com>
2019-04-15 09:20:20 +0100

    is Active list-group

--- public/JS/main.js
+++ public/JS/main.js
@@ -41,3 +41,9 @@ $(document).ready(function() {
         $(this).find('.icon-change').toggleClass('fas');
     });
 });
+
+
+$('.list-group-item-action').click(function(e) {
+    e.preventDefault();
+    $(this).toggleClass('active');
+});
