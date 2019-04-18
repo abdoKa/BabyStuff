62c0f9e47c715bf3666cc108b15eca1535f30709
Ali Kapoor <abdellalikabou39@gmail.com>
2019-04-18 14:16:00 +0100

    fix bag_shoppong session

--- src/Controller/ShopingController.php
+++ src/Controller/ShopingController.php
@@ -6,32 +6,65 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Categorie;
 use App\Entity\Produit;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Request;
 
 class ShopingController extends AbstractController
 {
     /**
      * @Route("/panier", name="shoping")
      */
-    public function bag_shoping()
+    public function bag_shoping(Request $request)
     {
-        $em =$this->getDoctrine()->getManager();
-        $repoC =$em->getRepository(Categorie::class);
-        $categoriesMenu =$repoC->getCategories();
+        $session = new Session();
+        $total=0;
+        $listProducts=array();
+
+        $quantity=$request->request->get('qte');
+        $product_id=$request->request->get('product_id');
 
         
- 
-
-
-        // $productRepository = $em->getRepository(Produit::class);
-        // $productsArray = [];
-        // $cart = [];
-        // $totalSum = 0;
-
-        return $this->render('shoping/bag-shoping.html.twig', [
-            'categoriesMenu' =>$categoriesMenu
-            // 'productsArray'=>$productsArray
-
-           
+        $repository = $this->getDoctrine()->getRepository(Produit::class);
+        $product0=$repository->findOneBy([
+            'id' =>$product_id 
         ]);
+        $prix=$product0->getPrix();
+        $total_product=$prix * $quantity;
+        
+        $product=array(
+            'product_id'=>$product_id,
+            'quantity'=>$quantity,
+            'prix'=>$prix,
+            'total'=>$total_product
+        );
+    
+        $product1=array(
+            'product_id'=>1,
+            'quantity'=>1,
+            'prix'=>400,
+            'total'=>400
+        );
+
+        array_push($listProducts,$product,$product1);
+        
+        
+        
+        
+        foreach ($listProducts as $product )
+        {
+            $total+= $product['total'];
+        }
+         $cart[]=array(
+             'listproducts'=>$listProducts,
+             'total'=>$total
+         );
+        
+         if(!$session->get('my_cart'))
+         {
+             
+         }
+         dump($session);
+         die();
+        
     }
 }
