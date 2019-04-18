c5af36ec8f7cce9a2eaad8af730f55dd06812717
Ismail Sanni <sanni920@gmail.com>
2019-04-18 15:52:10 +0100

    session fix

--- src/Controller/ShopingController.php
+++ src/Controller/ShopingController.php
@@ -2,12 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Categorie;
 use App\Entity\Produit;
-use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ShopingController extends AbstractController
 {
@@ -16,61 +16,61 @@ class ShopingController extends AbstractController
      */
     public function bag_shoping(Request $request)
     {
-
-        if ($request->isMethod('post')) {
-            // your code
-            
-            $session = new Session();
-            
-            $cart=array();
-            
-            if($session->get('my_cart') == null)
-            {
-                $session->set('my_cart',$cart);
-            }else{
-                $cart=$session->get('my_cart');
-            }
-            $total=0;
-            $listProducts=array();
-            $quantity=$request->request->get('qte');
-            $product_id=$request->request->get('product_id');
-
-            $repository = $this->getDoctrine()->getRepository(Produit::class);
-            $product0=$repository->findOneBy([
-                'id' =>$product_id 
-            ]);
-            
-                $prix=$product0->getPrix();
-                $total_product=$prix * $quantity;
-                
-                $product=array(
-                    'product_id'=>$product_id,
-                    'quantity'=>$quantity,
-                    'prix'=>$prix,
-                    'total'=>$total_product
-                );
-            
+        $session = new Session();
         
-                array_push($listProducts,$product);
-                
-                foreach ($listProducts as $product )
-                {
-                    $total+= $product['total'];
-                }
-                 $cart=array(
-                     'listproducts'=>$listProducts,
-                     'total'=>$total
-                 );
-               
+
+        $cart = array();
+        $total = 0;
+        $listProducts = array();
+
+        if ($session->get('my_cart') == null) {
+            $session->set('my_cart', $cart);
+        } else {
+            $cart = $session->get('my_cart');
         }
 
-        $em =$this->getDoctrine()->getManager();
-        $repoC =$em->getRepository(Categorie::class);
-        $categoriesMenu =$repoC->getCategories();
+        if ($request->isMethod('post')) {
 
-         return $this->render('shopping/bag-shoping.html.twig',[
-            'categoriesMenu' =>$categoriesMenu,
+            $quantity = $request->request->get('qte');
+            $product_id = $request->request->get('product_id');
+
+            $repository = $this->getDoctrine()->getRepository(Produit::class);
+            $product0 = $repository->findOneBy([
+                'id' => $product_id,
+            ]);
+
+            $prix = $product0->getPrix();
+            $total_product = $prix * $quantity;
+
+            $product = array(
+                'product_name' => $product0->getNom(),
+                'product_id' => $product_id,
+                'quantity' => $quantity,
+                'prix' => $prix,
+                'total' => $total_product,
+            );
+
+            array_push($listProducts, $product);
+
+            foreach ($listProducts as $product) {
+                $total += $product['total'];
+            }
+            $cart = array(
+                'listproducts' => $listProducts,
+                'total' => $total,
+            );
+            $session->set('my_cart', $cart);
+
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $repoC = $em->getRepository(Categorie::class);
+        $categoriesMenu = $repoC->getCategories();
+        $session->invalidate();
+
+        return $this->render('shopping/bag-shoping.html.twig', [
+            'categoriesMenu' => $categoriesMenu,
         ]);
-        
+
     }
 }
