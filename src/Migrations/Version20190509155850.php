09a315c9db40b61069002289c98c19ea1a61b166
Ali Kapoor <abdellalikabou39@gmail.com>
2019-05-09 16:04:52 +0000

    settings part 1

--- src/Migrations/Version20190509155850.php
+++ src/Migrations/Version20190509155850.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190509151926 extends AbstractMigration
+final class Version20190509155850 extends AbstractMigration
 {
     public function getDescription() : string
     {
